7abb70381081fd19885a034b6e29e720c9f765f2
Jan Kot <jan@catnip.supply>
2026-06-28 01:58:39 +0200

    nvim: setup deno lsp

--- dot_config/nvim/lua/config/language-servers.lua
+++ dot_config/nvim/lua/config/language-servers.lua
@@ -3,6 +3,11 @@ return {
 		cmd = { 'efm-langserver' },
 		filetypes = { 'meson', 'javascript', 'typescript', 'hexpat', 'sh' }
 	},
+	["deno-lsp"] = {
+		cmd = { 'deno', 'lsp' },
+		filetypes = { 'javascript', 'typescript', 'typescriptreact' },
+		root_markers = { 'deno.json' }
+	},
 	["nixd"] = {
 		cmd = { 'nixd' },
 		filetypes = { 'nix' },
