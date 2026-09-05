a600e8e484ccce86fb64f62f42ebc84886cc519e
Jan Kot <jan@catnip.supply>
2026-09-05 23:00:13 +0200

    nvim: adjust indentation settings

--- dot_config/nvim/lua/config/opts.lua
+++ dot_config/nvim/lua/config/opts.lua
@@ -1,7 +1,7 @@
 return {
-	autoindent = false,
 	colorcolumn = '80,120',
 	cursorline = true,
+	expandtab = true,
 	hlsearch = false,
 	ignorecase = true,
 	incsearch = true,
@@ -10,13 +10,11 @@ return {
 	number = true,
 	numberwidth = 6,
 	relativenumber = true,
-	shiftwidth = 4 ,
+	shiftwidth = 4,  -- indentation width
+	softtabstop = 2, -- spaces per tab key
 	-- showmode = false,
 	signcolumn = 'yes',
 	smartcase = true,
-	smartindent = true,
-	smarttab = true,
-	tabstop = 4,
 	termguicolors = true,
 	wrap = false
 }
